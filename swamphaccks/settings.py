8484a87d5c3688a805f69ac11d2adc76cebac151
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-01-31 12:42:39 -0500

    settings.py change

--- swamphaccks/settings.py
+++ swamphaccks/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+import os, django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django.apps
